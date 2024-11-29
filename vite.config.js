19a4167d4881dda9f198a03542ebdf510cb64c1c
sanabel-al-firdaws <abdullhakim.sami@gmail.com>
2024-11-29 05:09:41 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import gleam from "vite-gleam";
 
+import { VitePWA } from 'vite-plugin-pwa'
+
+
 export default {
-    plugins: [gleam()],
+    plugins: [gleam(), VitePWA({
+        workbox: {
+            globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        }, injectRegister: 'script-defer', registerType: 'autoUpdate'
+    })
+    ],
     build: {
         rollupOptions: {
-            input: 'main.js',
+            input: 'index.html',
             output: {
                 dir: './dist',
                 entryFileNames: 'assets/gleam_vite.js',
